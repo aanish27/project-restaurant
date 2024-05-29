168f15825dc07ea4c3bc8b54a25563dfdd54db95
axnish27 <itsaanish10@gmail.com>
2024-05-29 17:43:16 +0530

    tabbed browsing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 console.log("webpack Working")
 import img from "./assets/img.jpg"
-import { addContentToDiv } from "./modules/add-content-to-div";
+import { contactContent,aboutContent,homeContent } from "./modules/add-content-to-div";
 
 const content = document.querySelector("#content")
 
 
+const about = document.querySelector('.about')
+const home = document.querySelector('.home')
+const contact = document.querySelector('.contact')
+
+about.addEventListener('click' , () => {
+    if (content.firstChild) {
+        content.querySelectorAll("*").forEach(e => e.remove());
+    }
+    aboutContent(content,img)
+})
+home.addEventListener('click' , () => {
+    if (content.firstChild) {
+        content.querySelectorAll("*").forEach(e => e.remove());
+    }
+    homeContent(content,img)
+})
+
+contact.addEventListener('click' , () => {
+    if (content.firstChild) {
+        content.querySelectorAll("*").forEach(e => e.remove());
+    }
+    contactContent(content,img)
+})
 
 
-addContentToDiv(content,img)
+
+
--- src/modules/add-content-to-div.js
+++ src/modules/add-content-to-div.js
@@ -1,10 +1,42 @@
-export function addContentToDiv(div,img) {
+export { homeContent , aboutContent , contactContent };
+
+
+function homeContent(div,img) {
     let bgImg = document.createElement("img")
     bgImg.src = img
+    bgImg.style.width = '600px'
+    bgImg.style.height = '600px'
     let introText = document.createElement("p")
     let openText = document.createElement("p")
     introText.innerHTML = "we are the best in the west"
     openText.innerHTML = "we are oben from 7am to 9pm, come visit sheck out us"
+    return div.append(bgImg,introText,openText)
+
+}
+
+
+function aboutContent(div,img) {
+    let bgImg = document.createElement("img")
+    bgImg.src = img
+    bgImg.style.width = '600px'
+    bgImg.style.height = '600px'
+    let introText = document.createElement("p")
+    let openText = document.createElement("p")
+    introText.innerHTML = "the story"
+    openText.innerHTML = "i dont even know"
+    return div.append(bgImg,introText,openText)
+
+}
+
+function contactContent(div,img) {
+    let bgImg = document.createElement("img")
+    bgImg.src = img
+    bgImg.style.width = '600px'
+    bgImg.style.height = '600px'
+    let introText = document.createElement("p")
+    let openText = document.createElement("p")
+    introText.innerHTML = "contact"
+    openText.innerHTML = "+94100023003"
     return div.append(bgImg,introText,openText)
 
 }
